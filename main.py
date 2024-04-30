406295d02d1a1b666cedde5bb0d819ca2c1152c5
yoezequiel <ezequielcasiano15@gmail.com>
2024-04-29 23:17:37 -0300

    add arg --camera off/on

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from flask import Flask, render_template, Response
 from camera import VideoCamera
 import flask
@@ -7,6 +8,8 @@ import socket
 
 app = Flask(__name__)
 
+camera_enabled = True  # Variable global para controlar el estado de la cámara
+
 
 @app.route("/")
 def index():
@@ -15,15 +18,21 @@ def index():
 
 def gen(camera):
     while True:
-        frame = camera.get_frame()
-        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n")
+        if camera_enabled:  # Solo obtener el frame si la cámara está activada
+            frame = camera.get_frame()
+            yield (
+                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n\r\n"
+            )
 
 
 @app.route("/video_feed")
 def video_feed():
-    return Response(
-        gen(VideoCamera()), mimetype="multipart/x-mixed-replace; boundary=frame"
-    )
+    if camera_enabled:  # Solo iniciar el feed si la cámara está activada
+        return Response(
+            gen(VideoCamera()), mimetype="multipart/x-mixed-replace; boundary=frame"
+        )
+    else:
+        return "Cámara desactivada"
 
 
 @app.route("/screen.png")
@@ -42,7 +51,7 @@ def send_js(path):
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # doesn't even have to be reachable
+        # no necesita ser alcanzable
         s.connect(("10.255.255.255", 1))
         IP = s.getsockname()[0]
     except:
@@ -53,4 +62,13 @@ def get_ip():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Control de cámara")
+    parser.add_argument(
+        "--camera", choices=["on", "off"], default="on", help="Estado de la cámara"
+    )
+    args = parser.parse_args()
+
+    if args.camera == "off":
+        camera_enabled = False
+
     app.run(host=get_ip(), debug=True)
